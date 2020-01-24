4eabf19a3a657c50ebfa4c98b90774371d2c7728
Austin Felipe <austin.santos@evoxcorp.com>
2020-01-24 03:22:08 -0300

    Fix param type

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,9 @@ export const makeChannel = function<TChannelType>() {
   return new Channel<TChannelType>();
 };
 
-export const goNode = function(routineFunction: any, ...args: any): void {
+export const goNode = function(routineFunction: any, ...args: any[]): void {
   new Routine(routineFunction).run(args);
 };
 
-function sum(s: number[], c: Channel<number>) {
-  let sum = 0;
-  for (const v of s) {
-    sum += v;
-  }
-  c.send(sum);
-}
-const s = [7, 2, 8, -9, 4, 0];
-const c = makeChannel<number>();
-
-goNode(sum, s.slice(0, s.length / 2), c);
-goNode(sum, s.slice(s.length / 2), c);
-
-const [x, y] = [c.receive(), c.receive()];
-
-console.log(x, y, x + y);
+export { Channel };
+export { Routine };
--- src/routine.ts
+++ src/routine.ts
@@ -1,9 +1,7 @@
-import { Worker } from 'worker_threads';
-
 export default class Routine {
   constructor(private routineFunction: any) {}
 
-  run(...args: any) {
+  run(...args: any[]) {
     this.routineFunction.apply(this, args);
   }
 }
