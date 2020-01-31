651f0fd98316f00560196b347232316195594c8e
Austin Felipe <austin.santos@evoxcorp.com>
2020-01-31 01:12:58 -0300

    Implement worker call

--- src/routine.ts
+++ src/routine.ts
@@ -1,7 +1,25 @@
+import { Worker } from 'worker_threads';
+
 export default class Routine {
+  private worker?: Worker = undefined;
+
   constructor(private routineFunction: any) {}
 
   run(...args: any[]) {
-    this.routineFunction.apply(this, args);
+    // this.routineFunction.apply(this, args);
+    this.worker = new Worker('./worker-handler.js', {
+      workerData: {
+        routineFunction: this.routineFunction,
+        args,
+      },
+    });
+  }
+
+  getWorker() {
+    return this.worker;
   }
 }
+
+export const go = function(routineFunction: any, ...args: any[]): void {
+  new Routine(routineFunction).run(args);
+};
